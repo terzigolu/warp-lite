8028d283d3b00f5665d50b8d9dc24a0bdcca927b
Andy <8334252+acarl005@users.noreply.github.com>
2026-05-04 10:49:56 -0700

    fix: disable reset grid checks for restored blocks on Windows (#9987)
    (cherry picked from commit a9a5b6af3a8a69b3b48a32659b2ef63456f792dc)

--- app/src/terminal/model/blocks.rs
+++ app/src/terminal/model/blocks.rs
@@ -2636,6 +2636,7 @@ impl BlockList {
 
         // Start the block and add the command
         self.active_block_mut().start();
+        self.active_block_mut().disable_reset_grid_checks();
         processor.parse_bytes(self, command.as_bytes(), &mut io::sink());
 
         // Simulate preexec to transition to Executing state
@@ -2880,6 +2881,7 @@ impl BlockList {
             self.active_block_mut().start_background(None);
         } else {
             self.active_block_mut().start();
+            self.active_block_mut().disable_reset_grid_checks();
         }
 
         if let Some(serialized_ai_metadata) = block.ai_metadata.as_ref().and_then(|ai_metadata| {
